b8b7ac18403f8953f407b81e33f6ab6c3bbb4fa8
Zach Heineman <zach@heineman.info>
2017-01-07 22:35:08 -0800

    Working version

---
+++ make.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var moment = require('moment');
+
+var numWeeks = 52;
+var weekOne = moment('2017-01-02');
+
+for (var i = 0; i < numWeeks; i++) {
+
+  var weekNumber = i > 9 ? "" + i: "0" + i;
+  var start = moment().add(i, 'weeks').startOf('isoWeek')
+  var end = moment().add(i, 'weeks').endOf('isoWeek')
+
+  var pathName = weekNumber + '  ' + start.format("MMM") + ' ' + start.format("DD") + ' - ' + end.format("MMM") + ' ' + end.format("DD");
+  var path = '2017/' + pathName;
+
+  fs.mkdir(path);
+
+}
